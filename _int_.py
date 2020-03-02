bee062cd7411b59eb5f094d507ae700326023f05
Kagami228 <43517039+Kagami228@users.noreply.github.com>
2020-03-02 20:33:51 +0200

    Create _int_.py

---
+++ _int_.py
@@ -0,0 +1,3 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater('1003753572:AAGQySVdx1NyTPyRqb538UtsYXuiFot5h94',use_context=True)
+dispatcher = updater.dispatcher
