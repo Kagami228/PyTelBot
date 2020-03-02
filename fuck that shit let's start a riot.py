15a1dc67ace4b561ed84b2b548bbd6df5c9deaec
maxxonich <60605832+maxxonich@users.noreply.github.com>
2020-03-02 20:55:13 +0200

    Update and rename fuck that shit let's start a riot to fuck that shit let's start a riot.py

--- fuck that shit let's start a riot.py
+++ fuck that shit let's start a riot.py
@@ -1,13 +1,13 @@
 # Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import apiai, json
-updater = Updater(token='ВАШ API ТОКЕН') # Токен API к Telegram
+updater = Updater(token='1003753572:AAGQySVdx1NyTPyRqb538UtsYXuiFot5h94') # Токен API к Telegram
 dispatcher = updater.dispatcher
 # Обработка команд
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 def textMessage(bot, update):
-    request = apiai.ApiAI('ВАШ API ТОКЕН').text_request() # Токен API к Dialogflow
+    request = apiai.ApiAI('1003753572:AAGQySVdx1NyTPyRqb538UtsYXuiFot5h94').text_request() # Токен API к Dialogflow
     request.lang = 'ru' # На каком языке будет послан запрос
     request.session_id = 'BatlabAIBot' # ID Сессии диалога (нужно, чтобы потом учить бота)
     request.query = update.message.text # Посылаем запрос к ИИ с сообщением от юзера
